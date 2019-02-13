77ce9a29d7ce2e109e33034b03aa515795b024af
AidenTemere <aidentemere@gmail.com>
2019-02-13 22:31:55 +0200

    Added Bootstrap and main page

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>StudentLogger: Login</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container text-center">
+  <h1>StudentLogger</h1>
+</div>
+
+<body>
+</html>
