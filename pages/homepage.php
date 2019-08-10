9e39352418d210c47ece87d590f0dd5ee8d2bbbe
AidenTemere <aidentemere@gmail.com>
2019-08-10 16:30:19 +0100

    Completed homepage

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +6,7 @@
   <meta charset="utf-8">
   <meta name= "viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../css/style.css?version=2">
+  <link rel="stylesheet" href="../css/style.css?version=3">
   <link rel="icon" href="../resources/favicon.ico">
 </head>
 <body>
@@ -25,5 +26,34 @@
       </ul>
     </div>
   </nav>
+  <div class="container text-size-m">
+    <div id="welcome" class="text-center">
+      Welcome back, <?php echo $_SESSION['usr'];?>
+    </div>
+    <div class="row row-padded">
+      <div class="col-lg-6 text-center">
+        <a class="btn btn-primary btn-mega" href="account.php">
+          <p><img src="../resources/account.png"><br>Account</p>
+        </a>
+      </div>
+      <div class="col-lg-6 text-center">
+        <a class="btn btn-success btn-mega" href="manage_students.php">
+          <p><img src="../resources/manage_students.png"><br>Manage Students</p>
+        </a>
+      </div>
+    </div>
+    <div class="row row-padded">
+      <div class="col-lg-6 text-center">
+        <a class="btn btn-danger btn-mega" href="student_data.php">
+          <p><img src="../resources/student_data.png"><br>Student Data</p>
+        </a>
+      </div>
+      <div class="col-lg-6 text-center">
+        <a class="btn btn-warning btn-mega" href="data_representation.php">
+          <p><img src="../resources/data_representation.png"><br>Data Representation</p>
+        </a>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
