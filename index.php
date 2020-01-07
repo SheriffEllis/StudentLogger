e7f80462e3edc483231b40f795673817f36d31fa
AidenTemere <aidentemere@gmail.com>
2020-01-07 18:30:05 +0200

    Error box adjustments.

--- index.php
+++ index.php
@@ -29,57 +29,45 @@ unset($_SESSION['pwd']);
     <img src="/StudentLogger/resources/logo.png">
     <h2>User Login</h2>
     <p style="font-size: 15px">Please enter your login details</p>
-    <!--
-    The form data is sent to a separate php script file
-    which judges the validity of the login data
-    -->
+    <!-- The form data is sent to a separate php script file which judges the validity of the login data -->
     <form action="/StudentLogger/php/process_login.php" method="post">
       <div class="form-group">
         <label for="usr" class="sr-only">Username</label>
         <input type="username" class="col-lg-4 col-centered form-control form-m" placeholder="Enter username" id="login-field"
         name="usr" autocomplete="<?php
-        //Auto input username if user clicked "Remember me" tickbox
-        if(empty($_SESSION['remember'])){
-          echo "false";
-        }else{
-          echo "username";
-        }
+          //Auto input username if user clicked "Remember me" tickbox
+          if(empty($_SESSION['remember'])){
+            echo "false";
+          }else{
+            echo "username";
+          }
         ?>">
       </div>
-      <?php
-      //TODO: use javascript style.display = "none"/"block" instead?
-      //Show errorbox when login algorithm returns that username does not exist
-      if(!empty($_SESSION['wrng_usr'])){echo "
-        <div class='col-centered error-box'>
-          Username does not exist
-        </div>
-        ";}
-      ?>
+      <div class="col-centered error-box <?php if(empty($_SESSION['wrng_usr'])){echo 'hidden';} ?>">
+        Username does not exist
+      </div>
+      <!-- Show errorbox when login algorithm returns that username does not exist -->
+
       <div class="form-group">
         <label for="pwd" class="sr-only">Password</label>
         <input type="password" class="col-lg-4 col-centered form-control form-m" placeholder="Enter password" id="login-field"
         name="pwd" autocomplete="<?php
-        //Auto input password if user clicked "Remember me" tickbox
-        if(empty($_SESSION['remember'])){
-          echo "false";
-        }else{
-          echo "password";
-        }
+          //Auto input password if user clicked "Remember me" tickbox
+          if(empty($_SESSION['remember'])){
+            echo "false";
+          }else{
+            echo "password";
+          }
         ?>">
       </div>
-      <?php
-      //Show errorbox when login algorithm returns that password is invalid
-      if(!empty($_SESSION['wrng_pwd'])){echo "
-        <div class='col-centered error-box'>
-          Incorrect password, please try again
-        </div>
-        ";}
-      ?>
+      <div class="col-centered error-box <?php if(empty($_SESSION['wrng_pwd'])){echo 'hidden';} ?>">
+        Incorrect password, please try again
+      </div>
+      <!-- Show errorbox when login algorithm returns that password is invalid -->
+
       <div class="checkbox">
-        <label><input type="checkbox" name="remember"<?php
-        //Retain if "Remember me" tickbox was checked
-        if(!empty($_SESSION['remember'])){echo "checked";}
-        ?>> Remember me</label>
+        <!-- Retain if "Remember me" tickbox was checked -->
+        <label><input type="checkbox" name="remember" <?php if(!empty($_SESSION['remember'])){echo "checked";} ?>> Remember me</label>
       </div>
       <button type="submit" class="btn btn-primary">Log in</button>
     </form>
--- pages/sign_up_page.php
+++ pages/sign_up_page.php
@@ -22,65 +22,44 @@ unset($_SESSION['wrng_usr']);
     <img src="/StudentLogger/resources/logo.png">
     <h2>Sign up</h2>
     <p style="font-size: 15px">Please enter your details to create an account</p>
-    <!--
-    The form data is sent to a separate php script file
-    which judges the validity of the signup data
-    -->
+    <!-- The form data is sent to a separate php script file which judges the validity of the signup data -->
     <form action="/StudentLogger/php/process_signup.php" method="post">
       <div class="form-group">
         <label for="email" class="sr-only">Email</label>
         <input type="email" class="col-lg-4 col-centered form-control form-m" placeholder="Enter email address" id=login-field
-        name="email" autocomplete="email" value="<?php
-          //Fill in email if it was entered before
-          if(!empty($_SESSION['email'])){echo $_SESSION['email'];}
-        ?>">
+        name="email" autocomplete="email" value="<?php if(!empty($_SESSION['email'])){echo $_SESSION['email'];} ?>">
+        <!-- email is filled in if it was entered before -->
       </div>
-      <?php
-        //TODO: use javascript style.display = "none"/"block" instead?
-        //Show errorbox when signup algorithm returns that email is invalid
-        if(!empty($_SESSION['invld_email'])){echo "
-          <div class='col-centered error-box'>
-            <b>Email invalid:</b> must be less than 320 characters and a real email address.
-          </div>
-          ";}
-      ?>
+      <div class="col-centered error-box <?php if(empty($_SESSION['invld_email'])){echo 'hidden';} ?>">
+        <b>Email invalid:</b> must be less than 320 characters and a real email address.
+      </div>
+      <!-- Show errorbox when signup algorithm returns that email is invalid -->
+
       <div class="form-group">
         <label for="usr" class="sr-only">Username</label>
         <input type="username" class="col-lg-4 col-centered form-control form-m" placeholder="Enter username" id=login-field
-        name="usr" autocomplete="username" value="<?php
-          //Fill in username if it was entered before
-          if(!empty($_SESSION['usr'])){echo $_SESSION['usr'];}
-        ?>">
+        name="usr" autocomplete="username" value="<?php if(!empty($_SESSION['usr'])){echo $_SESSION['usr'];} ?>">
+        <!-- username is filled in if it was entered before -->
       </div>
-      <?php
-        //Show errorbox when signup algorithm returns that username is invalid
-        if(!empty($_SESSION['invld_usr'])){echo "
-          <div class='col-centered error-box'>
-            <b>Username invalid:</b> must be less than 50 characters and contain no special characters.
-          </div>
-          ";}
-        if(!empty($_SESSION['usr_unavailable'])){echo "
-          <div class='col-centered error-box'>
-            <b>Username invalid:</b> this username has already been taken.
-          </div>
-          ";}
-      ?>
+      <div class="col-centered error-box <?php if(empty($_SESSION['invld_usr'])){echo 'hidden';} ?>">
+        <b>Username invalid:</b> must be less than 50 characters and contain no special characters.
+      </div>
+      <div class="col-centered error-box <?php if(empty($_SESSION['usr_unavailable'])){echo 'hidden';} ?>">
+        <b>Username invalid:</b> this username has already been taken.
+      </div>
+      <!-- Show errorbox when signup algorithm returns that username is invalid -->
+
       <div class="form-group">
         <label for="pwd" class="sr-only">Password</label>
         <input type="password" class="col-lg-4 col-centered form-control form-m" placeholder="Enter password" id=login-field
-        name="pwd" autocomplete="new-password" value="<?php
-          //Fill in password if it was entered before
-          if(!empty($_SESSION['pwd'])){echo $_SESSION['pwd'];}
-        ?>">
+        name="pwd" autocomplete="new-password" value="<?php if(!empty($_SESSION['pwd'])){echo $_SESSION['pwd'];} ?>">
+        <!-- password is filled in if it was entered before -->
       </div>
-      <?php
-        //Show errorbox when signup algorithm returns that password is invalid
-        if(!empty($_SESSION['invld_pwd'])){echo "
-          <div class='col-centered error-box'>
-            <b>Password invalid:</b> must contain at least one number, one lower and upper case letter, and no white spaces.
-          </div>
-          ";}
-      ?>
+      <div class="col-centered error-box <?php if(empty($_SESSION['invld_pwd'])){echo 'hidden';} ?>">
+        <b>Password invalid:</b> must contain at least one number, one lower and upper case letter, and no white spaces.
+      </div>
+      <!-- Show errorbox when signup algorithm returns that password is invalid -->
+      
       <button type="submit" class="btn btn-primary">Sign up</button>
     </form>
     <a href="/StudentLogger/index.php" class="btn btn-link" role="button">Already have an account? Login</a>
