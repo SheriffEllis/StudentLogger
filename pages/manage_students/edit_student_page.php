f61e6a64fc5c34806511cf2437420b9fb1ce3e2e
AidenTemere <aidentemere@gmail.com>
2020-01-08 17:46:40 +0200

    Created Edit/Create Student Page

---
+++ pages/manage_students/edit_student_page.php
@@ -0,0 +1,39 @@
+<?php
+  session_start();
+  $title = 'Edit Student';
+  $web_section = 'manage_students';
+  $current_path = getenv('CURRENT_PATH');
+
+  $conn = new mysqli(getenv('HTTP_HOST'), getenv('HTTP_USER'), getenv('HTTP_PASS'), getenv('HTTP_DATABASE'));
+  if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+  }
+
+  $fields = array(); //array to fill with field names
+  $values = array(); //array to fill with values corresponding to fields for existing student
+
+  //Each fetched row has keys: Field, Type, Null, Key, Default, Extra
+  $sql = 'SHOW COLUMNS FROM student';
+  $get_fields = $conn->query($sql);
+  while($row = $get_fields->fetch_assoc()){
+    array_push($fields, $row['Field']);
+  }
+
+  //TODO: insert actual student ID selected from manage students page
+  $Student_ID = 1;
+  $stmt = $conn->prepare('SELECT * FROM student WHERE Student_ID=?');
+  $stmt->bind_param('i', $Student_ID);
+  $stmt->execute();
+  $get_values = $stmt->get_result();
+  //fetch only the first row; there shouldn't be more, but if there are, they'll be ignored
+  $values = $get_values->fetch_assoc(); //Associative array of values
+  $stmt->close();
+  $conn->close();
+
+  require($current_path . '/templates/navbar.php');
+  require($current_path . '/templates/entering_student_data.php')
+?>
+
+  <div id="buffer-box"></div>
+</body>
+</html>
---
+++ templates/entering_student_data.php
@@ -0,0 +1,30 @@
+<div class="container text-center">
+  <!-- Post results to self -->
+  <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+    <?php
+      foreach($fields as $field){
+        $readonly = ''; //used to make Student_ID readonly in edit page
+        if(!empty($values)){
+          $value = $values[$field];
+          if($field == 'Student_ID'){$readonly = "readonly";}
+        }else{
+          $value = '';
+        }
+
+        echo '
+        <div class="row form-group">
+          <label for="' . $field . '" class="label-text text-center">' . $field .'</label>
+          <input type="text" class="col-lg-4 col-centered form-control form-m" id="login-field"
+          name="' . $field . '" value="' . $value . '" ' . $readonly . '>
+        </div>
+        ';
+      }
+    ?>
+
+    <div class="row row-padded">
+      <!-- TODO: create/edit student script -->
+      <button class="btn btn-success btn-regular" type="submit">Save Changes</button>
+      <a class="btn btn-primary btn-regular" href="manage_students_page.php">Back</a>
+    </div>
+  </form>
+</div>
