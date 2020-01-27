e6a067bbea038868b790169ea39b51fbe7113ce5
AidenTemere <aidentemere@gmail.com>
2020-01-27 19:44:07 +0200

    Small adjustment to requiring Class_ID

--- templates/entering_student_data.php
+++ templates/entering_student_data.php
@@ -5,6 +5,7 @@
       foreach($fields as $field){
         $readonly = ''; //used to make Student_ID (and other ID's) readonly in edit page
         $hidden = ''; //used to make Class_ID hidden (is autoincremented)
+        $required = 'required'; //should be set to 'required' for all fields except Class_ID
 
         //Don't require entry of teacher: skip this entry
         if($field == 'Username'){
@@ -12,7 +13,10 @@
         }
 
         //Do not show the Class_ID input
-        if($field == 'Class_ID'){$hidden = 'style="display: none;"';}
+        if($field == 'Class_ID'){
+          $hidden = 'style="display: none;"';
+          $required = '';
+        }
 
         //If on edit page, do not allow edit of Student ID
         if(!empty($values)){
@@ -26,7 +30,7 @@
         <div class="row form-group">
           <label for="'. $field.'" class="label-text text-center"'.$hidden.'>'.$field.'</label>
           <input type="text" class="col-lg-4 col-centered form-control form-m"
-          name="'.$field.'" value="'.$value.'" '.$readonly.' '.$hidden.' required>
+          name="'.$field.'" value="'.$value.'" '.$readonly.' '.$hidden.' '.$required.'>
         </div>
         ';
       }
