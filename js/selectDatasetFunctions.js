2ee655241d0c8281f3641cbe0045d0179d39241a
AidenTemere <AidenTemere@users.noreply.github.com>
2020-02-02 17:56:34 +0200

    Implemented dynamic condition selection.

--- js/selectDatasetFunctions.js
+++ js/selectDatasetFunctions.js
@@ -6,11 +6,14 @@ function renderOptions(){
   var outputFieldSelect = $('#outputFieldSelect');
   var orderDeterminingRow = $('#orderDeterminingRow');
   var orderDeterminingFieldSelect = $('#orderDeterminingFieldSelect');
+  var conditionsSection = $('#conditionsSection');
 
   $.post('/StudentLogger/php/retrieve_table_fields.php', {table: selectedTable},
     function(tableFields){
       outputFieldSelect.empty();
       orderDeterminingFieldSelect.empty();
+      conditionsSection.empty();
+      addCondition(0);
       outputFieldSelect.append('<option disabled selected hidden>Select Output Field</option>');
       orderDeterminingFieldSelect.append('<option disabled selected hidden>Order Determining Field</option>');
       $.each(tableFields, function(index, value){
@@ -22,4 +25,81 @@ function renderOptions(){
       orderDeterminingRow.show();
     }, 'json'
   );
+}
+
+function addCondition(number){
+  var selectedTable = $('#tableSelect').val();
+  if(!selectedTable){alert('Error: no table selected'); return;}
+
+  var conditionsSection = $('#conditionsSection');
+  $.post('/StudentLogger/php/retrieve_table_fields.php', {table : selectedTable},
+    function(tableFields){
+      var optionString = '';
+      $.each(tableFields, function(index, value){
+        optionString = optionString.concat(`<option value="${value}">${value}</option>`);
+      });
+      conditionsSection.append(`
+        <div id="condition${number}">
+          <div class="col-lg-8 col-centered box">
+            <div class="row row-padded label-text text-center">
+              <label>Condition ${number}</label>
+              <input id="checkbox${number}" name="ticked[${number}]" class="mediumCheckbox" type="checkbox"
+              onchange="updateConditions()"></input>
+            </div>
+
+            <div class="row row-padded text-center">
+              <select name="investigated_field[${number}]" class="output-text">
+                <option disabled selected hidden>Investigated Field</option>
+                ${optionString}
+              </select>
+            </div>
+
+            <div class="row row-mid-padded text-center">
+              <select name="comparator[${number}]" class="output-text">
+                <option disabled selected hidden>Comparator</option>
+                <option value="=="> == </option>
+                <option value="!="> != </option>
+                <option value="<" > <  </option>
+                <option value="<="> <= </option>
+                <option value=">" > >  </option>
+                <option value="=>"> => </option>
+              </select>
+            </div>
+
+            <div class="row row-mid-padded">
+              <p class="col-lg-6 text-right output-text">Compared Value:</p>
+              <input name="compared_value[${number}]" class="col-lg-3 vertical-text-padding" placeholder="Enter value..."></input>
+            </div>
+          </div>
+
+          <div class="row row-mid-padded text-center">
+            <select name="logic_operator[${number}]" class="col-centered output-text">
+              <option selected disabled hidden>Logical Operator</option>
+              <option value="AND">AND</option>
+              <option value="OR">OR</option>
+              <option value="XOR">XOR</option>
+              <option value="NAND">NAND</option>
+              <option value="NOR">NOR</option>
+            </select>
+          </div>
+        </div>
+      `);
+    }, 'json'
+  );
+}
+
+function updateConditions(){
+  //checks if each condition box exists and if the box is checked
+  for(i = 0; $(`#condition${i}`).length; i++){
+    if($(`#checkbox${i}`).is(':checked')){         //If checkbox ticked
+      if(!$(`#condition${i+1}`).length){  //And next condition doesn't exist
+        addCondition(i+1);                //Create new condition
+      }
+    }else if($(`#condition${i+1}`).length){ //If checkbox unticked and next condition does exist
+      //loop through all consecutive conditions and remove
+      for(x = i+1; $(`#condition${x}`).length; x++){
+        $(`#condition${x}`).remove();
+      }
+    }
+  }
 }
--- pages/data_representation/select_dataset_page.php
+++ pages/data_representation/select_dataset_page.php
@@ -18,7 +18,6 @@
   require($current_path . '/templates/navbar.php');
   /*
   TODO:
-    Configure dynamic selection of conditions
     Submit->create dataset->add dataset id to Dataset_IDs->Submit data
     Cancel->Resubmit data
     if user cancels creation of table, all prior created datasets should be deleted
@@ -26,9 +25,27 @@
 ?>
 
   <form action="<?php echo $Postback; ?>" method="post">
+    <!-- hidden part of form resubmitting data to datatable/function -->
+    <div style="display: none;">
+      <?php
+        if($Postback == 'create_table_page.php'){
+          echo '
+            <input name="Table_title" value="'.$Table_title.'"/>
+            <input name="Table_description" value="'.$Table_description.'"/>
+            <input name="Dataset_index" value="'.$Dataset_index.'"/>
+          ';
+          foreach($Dataset_IDs as $index=>$ID){
+            echo '<input name="Dataset_IDs['.$index.']" value="'.$ID.'"/>';
+          }
+        }else{
+          //TODO
+        }
+      ?>
+    </div>
+
     <div class="container box">
       <div class="row row-padded text-center">
-        <select id="tableSelect" onchange="renderOptions()" class="col-centered output-text">
+        <select id="tableSelect" name="selected_table" onchange="renderOptions()" class="col-centered output-text">
           <option disabled selected hidden>Select Table</option>
           <?php
             //Select all table names except dataset and teacher
@@ -43,103 +60,23 @@
       </div>
 
       <div id="outputFieldRow" class="row row-padded text-center" style="display: none;">
-        <select id="outputFieldSelect" class="col-centered output-text">
+        <select id="outputFieldSelect" name="output_field" class="col-centered output-text">
         </select>
       </div>
 
       <div id="orderDeterminingRow" class="row row-padded text-center" style="display: none;">
-        <select id="orderDeterminingFieldSelect" class="output-text">
+        <select id="orderDeterminingFieldSelect" name="order_field" class="output-text">
         </select>
-        <select class="output-text">
+        <select name="ascending" class="output-text">
           <option disabled selected hidden>Order</option>
-          <option value="true">Ascending</option>
-          <option value="false">Descending</option>
+          <option value=true>Ascending</option>
+          <option value=false>Descending</option>
         </select>
       </div>
 
-      <!-- First condition box -->
-      <!-- TODO: implement dynamic adding of more condition boxes -->
       <div class="row row-padded">
-        <div class="col-lg-8 col-centered box">
-          <div class="row row-padded label-text text-center">
-            <label>Condition 1</label>
-            <input class="mediumCheckbox" type="checkbox"></input>
-          </div>
-
-          <div class="row row-padded text-center">
-            <select class="output-text">
-              <option disabled selected hidden>Investigated Field</option>
-              <option>Field1</option>
-              <option>Field2</option>
-              <option>Field3</option>
-            </select>
-          </div>
-
-          <!-- with adequete comparators, the "not" option isn't necessary -->
-
-          <div class="row row-mid-padded text-center">
-            <select class="output-text">
-              <option disabled selected hidden>Comparator</option>
-              <option> == </option>
-              <option> != </option>
-              <option> <  </option>
-              <option> <= </option>
-              <option> >  </option>
-              <option> => </option>
-            </select>
-          </div>
-
-          <div class="row row-mid-padded">
-            <p class="col-lg-6 text-right output-text">Compared Value:</p>
-            <input class="col-lg-3 vertical-text-padding" type="text" placeholder="Enter value..."></input>
-          </div>
+        <div id="conditionsSection">
         </div>
-
-        <div class="row row-mid-padded text-center">
-          <select class="col-centered output-text">
-            <option selected disabled hidden>Logical Operator</option>
-            <option>AND</option>
-            <option>OR</option>
-            <option>XOR</option>
-            <option>NAND</option>
-            <option>NOR</option>
-          </select>
-        </div>
-
-        <!-- Condition 2 -->
-        <div class="col-lg-8 col-centered box">
-          <div class="row row-padded label-text text-center">
-            <label>Condition 2</label>
-            <input class="mediumCheckbox" type="checkbox"></input>
-          </div>
-
-          <div class="row row-padded text-center">
-            <select class="output-text">
-              <option disabled selected hidden>Investigated Field</option>
-              <option>Field1</option>
-              <option>Field2</option>
-              <option>Field3</option>
-            </select>
-          </div>
-
-          <div class="row row-mid-padded text-center">
-            <select class="output-text">
-              <option disabled selected hidden>Comparator</option>
-              <option> == </option>
-              <option> != </option>
-              <option> <  </option>
-              <option> <= </option>
-              <option> >  </option>
-              <option> => </option>
-            </select>
-          </div>
-
-          <div class="row row-mid-padded">
-            <p class="col-lg-6 text-right output-text">Compared Value:</p>
-            <input class="col-lg-3 vertical-text-padding" type="text" placeholder="Enter value..."></input>
-          </div>
-        </div>
-
 
         <!-- TODO: make submit button actuall submit relevant data -->
         <div class="row row-padded text-center">
